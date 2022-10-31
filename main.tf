2d1d3ad654f51d2d807e5d0b74180e668908cd86
Predrag Vlajkovic <predrag.vlajkovic@gmail.com>
2022-10-31 13:36:41 +0100

    using v 1.0.0

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "vpc" {
-  source      = "git::https://github.com/drpreag/terraform-modules.git//vpc?ref=master"
+  source      = "git::https://github.com/drpreag/terraform-modules.git//vpc?ref=1.0.0"
   vpc-name    = var.vpc-name
   vpc-cidr    = var.vpc-cidr
   az-count    = var.az-count
@@ -9,14 +9,14 @@ module "vpc" {
 }
 
 module "sg" {
-  source      = "git::https://github.com/drpreag/terraform-modules.git//security_groups?ref=master"
+  source      = "git::https://github.com/drpreag/terraform-modules.git//security_groups?ref=1.0.0"
   vpc-id      = module.vpc.vpc.id
   environment = var.environment
   company-ips = var.company-ips
 }
 
 # module "bastion" {
-#   source         = "git::https://github.com/drpreag/terraform-modules.git//ec2-bastion?ref=master"
+#   source         = "git::https://github.com/drpreag/terraform-modules.git//ec2-bastion?ref=1.0.0"
 #   eni            = module.vpc.bastion-eni
 #   vpc-name       = var.vpc-name
 #   security-group = module.sg.security-groups[0]
@@ -26,20 +26,20 @@ module "sg" {
 # }
 
 module "route53" {
-  source      = "git::https://github.com/drpreag/terraform-modules.git//route53?ref=master"
+  source      = "git::https://github.com/drpreag/terraform-modules.git//route53?ref=1.0.0"
   vpc-id      = module.vpc.vpc.id
   zone-name   = "wtf.com"
   environment = var.environment
 }
 
 # module "kms" {
-#   source      = "git::https://github.com/drpreag/terraform-modules.git//kms?ref=master"
+#   source      = "git::https://github.com/drpreag/terraform-modules.git//kms?ref=1.0.0"
 #   vpc-id      = module.vpc.vpc.id
 #   environment = var.environment
 # }
 
 module "s3" {
-  source          = "git::https://github.com/drpreag/terraform-modules.git//s3?ref=master"
+  source          = "git::https://github.com/drpreag/terraform-modules.git//s3?ref=1.0.0"
   vpc-id          = module.vpc.vpc.id
   allowed-origins = "https://wtf.com"
   bucket-name     = "wtf-com-test-bucket"
